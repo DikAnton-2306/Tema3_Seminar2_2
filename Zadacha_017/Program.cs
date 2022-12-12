2c970c9978da58109c5b984314604d860bfc71ee
Антон Дик <antondik@yandex.ru>
2022-12-12 19:00:56 +0300

    / Задача 17. Принимает координаты точки (X<>0, Y<>0) и выдаёт номер четверти плоскости, где находится точка.

---
+++ Zadacha_017/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 17. Принимает координаты точки (X<>0, Y<>0) и выдаёт номер четверти плоскости, где находится точка.
+Console.Clear();
+
+int DataEntryXY (string str)
+{
+    Console.Write(str);
+    //int number = Convert.ToInt32(Console.ReadLine());
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+void FindQvadnrant (int x, int y)
+{
+    if(x > 0 && y > 0) 
+        Console.WriteLine("Это первый квадрат");
+    if(x < 0 && y > 0) 
+        Console.WriteLine("Это второй квадрат");
+    if(x < 0 && y < 0) 
+        Console.WriteLine("Это третий квадрат");
+    if(x > 0 && y < 0) 
+        Console.WriteLine("Это четвертый квадрат");
+}
+
+int x =  DataEntryXY("Введите кординаты X = ");
+int y =  DataEntryXY("Введите кординаты Y = ");
+
+FindQvadnrant(x,y);
